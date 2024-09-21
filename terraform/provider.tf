e4f7fe228465064eb521af206acf5f9c37a51016
mjbxnzr <muhdiqmal1997@gmail.com>
2024-09-21 23:53:58 +0800

    Update K8s manifest

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,37 +1,7 @@
 provider "aws" {
-  region                      = "ap-southeast-1"
-  access_key                  = "null"
-  secret_key                  = "null"
-  s3_use_path_style           = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    apigateway     = "http://localhost:4566"
-    cloudformation = "http://localhost:4566"
-    cloudfront = "http://localhost:4566"
-    cloudwatch     = "http://localhost:4566"
-    dynamodb       = "http://localhost:4566"
-    es             = "http://localhost:4566"
-    firehose       = "http://localhost:4566"
-    iam            = "http://localhost:4566"
-    kinesis        = "http://localhost:4566"
-    lambda         = "http://localhost:4566"
-    route53        = "http://localhost:4566"
-    redshift       = "http://LOCALHOST:4566"
-    s3             = "http://localhost:4566"
-    secretsmanager = "http://localhost:4566"
-    ses            = "http://localhost:4566"
-    sns            = "http://localhost:4566"
-    sqs            = "http://localhost:4566"
-    ssm            = "http://localhost:4566"
-    stepfunctions  = "http://localhost:4566"
-    sts            = "http://localhost:4566"
-    ec2            = "http://localhost:4566"
-    elbv2          = "http://localhost:4566"
-
-  }
+  region     = "ap-southeast-1"
+  access_key = "AKIA27VE4QSF4MPE2X77"
+  secret_key = "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"
 }
 
 terraform {
